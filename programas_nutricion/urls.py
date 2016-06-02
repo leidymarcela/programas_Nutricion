409e57e70e431877d8208ae32af12324d4816687
marcela1 <leidy-tangarife@upc.edu.co>
2016-06-02 16:07:58 -0700

    subiendo pagina

--- programas_nutricion/urls.py
+++ programas_nutricion/urls.py
@@ -58,6 +58,3 @@ urlpatterns = [
     url('^reportes$', reportes, name = 'reportes' )
 
     ]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
