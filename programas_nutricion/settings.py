f1f7f9348f4f4db42124ac65e83290dda4161992
marcela1 <leidy-tangarife@upc.edu.co>
2016-06-02 20:46:18 -0700

    subiendo pagina2

--- programas_nutricion/settings.py
+++ programas_nutricion/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR + '/static']
--- programas_nutricion/wsgi.py
+++ programas_nutricion/wsgi.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "programas_nutricion.settings")
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 from dj_static import Cling
 
 application = Cling(get_wsgi_application())
-
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
 
 
 
