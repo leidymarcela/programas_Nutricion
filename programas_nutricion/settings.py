5722acd7855f583f97048d20d4ca14e956b580eb
marcela1 <leidy-tangarife@upc.edu.co>
2016-10-06 16:44:50 -0700

    subiendo herocu

--- programas_nutricion/settings.py
+++ programas_nutricion/settings.py
@@ -87,25 +87,25 @@ WSGI_APPLICATION = 'programas_nutricion.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME': 'df67pkd10ktggs',
-        'USER':'zijzllkkagobtq',
-        'PASSWORD':'5BnL7SwTUmWeCaOm7HlMADyMbe',
-        'HOST': 'ec2-54-235-68-4.compute-1.amazonaws.com',
-        'PORT':'5432',
-    }
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'NAME': 'df67pkd10ktggs',
+#         'USER':'zijzllkkagobtq',
+#         'PASSWORD':'5BnL7SwTUmWeCaOm7HlMADyMbe',
+#         'HOST': 'ec2-54-235-68-4.compute-1.amazonaws.com',
+#         'PORT':'5432',
+#     }
+
+# }
 
 
 # Password validation
