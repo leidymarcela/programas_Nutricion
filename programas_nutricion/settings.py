d20cdeaa9984412cd64d53ba2cffd96fa34c96b4
marcela1 <leidy-tangarife@upc.edu.co>
2016-10-07 17:50:09 -0700

    conwindows

--- programas_nutricion/settings.py
+++ programas_nutricion/settings.py
@@ -86,25 +86,25 @@ WSGI_APPLICATION = 'programas_nutricion.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         'NAME': 'df67pkd10ktggs',
-#         'USER':'zijzllkkagobtq',
-#         'PASSWORD':'5BnL7SwTUmWeCaOm7HlMADyMbe',
-#         'HOST': 'ec2-54-235-68-4.compute-1.amazonaws.com',
-#         'PORT':'5432',
-#     }
-
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'postgresql-round-73501',
+        'USER':'qstxylpbwvmmgo',
+        'PASSWORD':'mMBWmS_ctx16NzBatuNFd1jZni',
+        'HOST': 'ec2-54-227-240-164.compute-1.amazonaws.com',
+        'PORT':'5432',
+    }
+
+}
 
 
 # Password validation
